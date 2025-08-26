42ea4b0e04eb8d115d62edb3ee3459bbfcee9bff
Elijah1123 <elijahmzalendo659@gmail.com>
2025-08-26 12:36:53 +0300

    python

---
+++ lib/dog.py
@@ -0,0 +1,10 @@
+class Dog:
+    def __init__(self, name, breed="Mutt" ):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting")        
---
+++ lib/person.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name):
+        self.name = name 
+        print(f"{name} is born!")
+
+    def talk(self):
+        print(f"Good Morning {name}?")
+
+    def walk(self):
+        print("The person is walking.")    
+
+
+Mzalendo = Person("Mzalendo")
+Zakayo = Person("Zakayo")
+Pabro = Person("Pabro")
+Lyn = Person("Lyn")
+Davi = Person("Davi")
